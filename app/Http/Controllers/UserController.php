a801c98bb0b4ae8f584ec9d425c04fae3fb0c12a
razabangi <muhammadrazabangi9@gmail.com>
2022-03-05 17:35:36 +0500

    add filters

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,20 @@ class UserController extends Controller
 {
     public function show()
     {
-        $users = User::with('posts')->get();
+        // filter | get users which have post
+        // $users = User::has('posts')->with('posts')->get();
+
+        // filter | get users which have post length greater than 2
+        // $users = User::has('posts', '>=', 2)->with('posts')->get();
+
+        // filter | get users which have this wild card title
+        // $users = User::whereHas('posts', function ($q) {
+        //     $q->where('title', 'like', "%Four%");
+        // })->with('posts')->get();
+
+        // filter | get users which have not post
+        // $users = User::doesntHave('posts')->with('posts')->get();
+
         return view("user", compact('users'));
     }
 }
